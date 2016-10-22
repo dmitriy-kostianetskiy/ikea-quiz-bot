9290161990a63465ff2e697d86c2cac42366cb10
redrick-tmn <redrick.tmn@gmail.com>
2016-10-22 19:07:29 +0000

    Another try to fix all connection issues

--- site/site.js
+++ site/site.js
@@ -12,51 +12,51 @@ class Site {
     constructor(server) {
 
         // view engine setup
-        server.app.set('views', path.join(__dirname, 'views'));
-        server.app.set('view engine', 'jade');
+        //server.app.set('views', path.join(__dirname, 'views'));
+        //server.app.set('view engine', 'jade');
 
         // uncomment after placing your favicon in /public
         //server.app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-        server.app.use(bodyParser.urlencoded({
-            extended: false
-        }));
+        //server.app.use(bodyParser.urlencoded({
+        //    extended: false
+        //}));
         
-        server.app.use(cookieParser());
+        //server.app.use(cookieParser());
         server.app.use(express.static(path.join(__dirname, 'public')));
 
-        indexRoutes(server);
+        //indexRoutes(server);
 
         // catch 404 and forward to error handler
-        server.app.use(function(req, res, next) {
-            var err = new Error('Not Found');
-            err.status = 404;
-            next(err);
-        });
+        // server.app.use(function(req, res, next) {
+        //     var err = new Error('Not Found');
+        //     err.status = 404;
+        //     next(err);
+        // });
 
         // error handlers
 
         // development error handler
         // will print stacktrace
-        if (server.app.get('env') === 'development') {
-            server.app.use(function(err, req, res, next) {
-                res.status(err.status || 500);
-                res.render('error', {
-                    message: err.message,
-                    error: err
-                });
-            });
-        }
+        // if (server.app.get('env') === 'development') {
+        //     server.app.use(function(err, req, res, next) {
+        //         res.status(err.status || 500);
+        //         res.render('error', {
+        //             message: err.message,
+        //             error: err
+        //         });
+        //     });
+        // }
 
         // production error handler
         // no stacktraces leaked to user
-        server.app.use((err, req, res, next) => {
-            res.status(err.status || 500);
-            res.render('error', {
-                message: err.message,
-                error: {}
-            });
-        });
+        // server.app.use((err, req, res, next) => {
+        //     res.status(err.status || 500);
+        //     res.render('error', {
+        //         message: err.message,
+        //         error: {}
+        //     });
+        // });
     }
 }
 
