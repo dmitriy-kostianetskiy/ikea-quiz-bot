8b9d400798173faff824564a89acb5fd20e39f10
redrick-tmn <redrick.tmn@gmail.com>
2016-10-22 18:57:34 +0000

    Another try to fix all connection issues

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 require("./storage/connection.js");
 let Bot = require('./app/bot.js');
 let Server = require('./server/server.js');
-let Site = require('./site/site.js');
+//let Site = require('./site/site.js');
 
 let server = new Server();
-let site = new Site(server);
+//let site = new Site(server);
 let bot = new Bot(server);
